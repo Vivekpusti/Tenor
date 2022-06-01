860cd7aa880c637263efa5e88c86a28a74d9b727
Vivekpusti <vivekpusti1999@gmail.com>
2022-06-01 18:15:39 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,149 @@
+from __future__ import division, print_function
+#import sys
+import os
+import cv2
+#import re
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from keras.preprocessing import image
+from keras.preprocessing import image
+from tensorflow.keras.utils import img_to_array
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+import statistics as st
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index1.html")
+    
+    
+@app.route('/camera', methods = ['GET', 'POST'])
+def camera():
+    i=0
+
+    GR_dict={0:(0,255,0),1:(0,0,255)}
+    model = tf.keras.models.load_model('mini.h5')
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    output=[]
+    cap = cv2.VideoCapture(0)
+    while (i<=30):
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray)
+
+        for x,y,w,h in faces:
+
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+            
+            if np.sum([roi_gray])!=0:
+                roi = roi_gray.astype('float')/255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi,axis=0)
+
+                predictions = model.predict(roi)[0]
+
+            max_index = np.argmax(predictions)
+
+            emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'neutral', 'surprise')
+            predicted_emotion = emotions[max_index]
+            output.append(predicted_emotion)
+            
+            
+            
+            cv2.rectangle(img,(x,y),(x+w,y+h),GR_dict[1],2)
+            cv2.rectangle(img,(x,y-40),(x+w,y),GR_dict[1],-1)
+            cv2.putText(img, predicted_emotion, (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+       
+        i = i+1
+
+        cv2.imshow('LIVE', img)
+        key = cv2.waitKey(1)
+        if key == 27: 
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+    print(output)
+    cap.release()
+    cv2.destroyAllWindows()
+    
+    final_output1 = st.mode(output)
+    return render_template("buttons.html",final_output=final_output1)
+
+
+@app.route('/templates/buttons', methods = ['GET','POST'])
+def buttons():
+    return render_template("buttons.html")
+
+@app.route('/templates/contact', methods = ['GET','POST'])
+def contact():
+    return render_template("contact.html")
+
+# @app.route('/movies/surprise', methods = ['GET', 'POST'])
+# def moviesSurprise():
+#     return render_template("moviesSurprise.html")
+
+# @app.route('/movies/angry', methods = ['GET', 'POST'])
+# def moviesAngry():
+#     return render_template("moviesAngry.html")
+
+# @app.route('/movies/sad', methods = ['GET', 'POST'])
+# def moviesSad():
+#     return render_template("moviesSad.html")
+
+# @app.route('/movies/disgust', methods = ['GET', 'POST'])
+# def moviesDisgust():
+#     return render_template("moviesDisgust.html")
+
+# @app.route('/movies/happy', methods = ['GET', 'POST'])
+# def moviesHappy():
+#     return render_template("moviesHappy.html")
+
+# @app.route('/movies/fear', methods = ['GET', 'POST'])
+# def moviesFear():
+#     return render_template("moviesFear.html")
+
+# @app.route('/movies/neutral', methods = ['GET', 'POST'])
+# def moviesNeutral():
+#     return render_template("moviesNeutral.html")
+
+@app.route('/songs/surprise', methods = ['GET', 'POST'])
+def songsSurprise():
+    return render_template("songsSurprise.html")
+
+@app.route('/songs/angry', methods = ['GET', 'POST'])
+def songsAngry():
+    return render_template("songsAngry.html")
+
+@app.route('/songs/sad', methods = ['GET', 'POST'])
+def songsSad():
+    return render_template("songsSad.html")
+
+@app.route('/songs/disgust', methods = ['GET', 'POST'])
+def songsDisgust():
+    return render_template("songsDisgust.html")
+
+@app.route('/songs/happy', methods = ['GET', 'POST'])
+def songsHappy():
+    return render_template("songsHappy.html")
+
+@app.route('/songs/fear', methods = ['GET', 'POST'])
+def songsFear():
+    return render_template("songsFear.html")
+
+@app.route('/songs/neutral', methods = ['GET', 'POST'])
+def songsNeutral():
+    return render_template("songsSad.html")
+
+@app.route('/templates/join_page', methods = ['GET', 'POST'])
+def join():
+    return render_template("join_page.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
